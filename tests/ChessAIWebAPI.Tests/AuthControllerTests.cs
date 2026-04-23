aeeefa20513cb07a1102152029155e0f1fbcc9b5
Davo0416 <60065631+Davo0416@users.noreply.github.com>
2026-04-23 22:34:44 +0100

    Tests

--- tests/ChessAIWebAPI.Tests/AuthControllerTests.cs
+++ tests/ChessAIWebAPI.Tests/AuthControllerTests.cs
@@ -10,39 +10,4 @@ using System.Threading.Tasks;
 
 public class AuthControllerTests
 {
-    private readonly Mock<IMongoService> _mongoMock;
-    private readonly AuthController _controller;
-
-    public AuthControllerTests()
-    {
-        _mongoMock = new Mock<IMongoService>();
-        _controller = new AuthController(_mongoMock.Object);
-    }
-
-    [Fact]
-    public async Task Login_ReturnsUnauthorized_WhenUserNotFound()
-    {
-        var mockUsers = new Mock<IMongoCollection<User>>();
-        var mockCursor = new Mock<IAsyncCursor<User>>();
-
-        mockCursor.Setup(x => x.Current).Returns(new List<User>());
-        mockCursor.SetupSequence(x => x.MoveNextAsync(default))
-                  .ReturnsAsync(false);
-
-        mockUsers.Setup(x =>
-            x.FindAsync(It.IsAny<FilterDefinition<User>>(),
-                        It.IsAny<FindOptions<User>>(),
-                        default))
-                 .ReturnsAsync(mockCursor.Object);
-
-        _mongoMock.Setup(x => x.Users).Returns(mockUsers.Object);
-
-        var result = await _controller.Login(new User
-        {
-            Username = "ghost",
-            PasswordHash = "123"
-        });
-
-        Assert.IsType<UnauthorizedObjectResult>(result);
-    }
 }
--- tests/ChessAIWebAPI.Tests/GameControllerTests.cs
+++ tests/ChessAIWebAPI.Tests/GameControllerTests.cs
@@ -9,55 +9,5 @@ using System.Collections.Generic;
 
 public class GameControllerTests
 {
-    private readonly Mock<IMongoService> _mongoMock;
-    private readonly GameController _controller;
-
-    public GameControllerTests()
-    {
-        _mongoMock = new Mock<IMongoService>();
-        _controller = new GameController(_mongoMock.Object);
-    }
-
-    [Fact]
-    public async Task SaveGame_ReturnsOk()
-    {
-        var game = new Game
-        {
-            Username = "test",
-            ClientId = "1"
-        };
-
-        // no mocking MongoDB collection anymore
-        _mongoMock.Setup(x => x.Games);
-
-        var result = await _controller.SaveGame(game);
-
-        Assert.IsType<OkResult>(result);
-    }
-
-    [Fact]
-    public async Task GetAll_ReturnsOkResult()
-    {
-        var games = new List<Game>
-        {
-            new Game { Username = "test", ClientId = "1" }
-        };
-
-        // you must mock controller logic instead of MongoDB
-        _mongoMock
-            .Setup(x => x.Games)
-            .Returns((MongoDB.Driver.IMongoCollection<Game>)null);
-
-        var result = await _controller.GetAll();
-
-        Assert.IsType<OkObjectResult>(result);
-    }
-
-    [Fact]
-    public async Task ClearUserGames_ReturnsOk()
-    {
-        var result = await _controller.ClearUserGames("test");
-
-        Assert.IsType<OkObjectResult>(result);
-    }
+   
 }
